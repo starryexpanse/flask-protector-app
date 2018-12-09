93cc46c890371debc42c280538c57f0e497d3cc7
Philip Peterson <philip.c.peterson@gmail.com>
2018-12-08 19:14:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from setuptools import setup
 setup(name='flask_protector_app',
       version='1.0',
       description='A Flask app to wrap other Flask apps, providing a login layer',
-      url='http://github.com/starryexpanse/flask_protector_app',
+      url='http://github.com/starryexpanse/flask-protector-app',
       author='Philip Peterson',
       author_email='pc.peterso@gmail.com',
       license='MIT',
       packages=['flask_protector_app'],
+      classifiers=(
+           'Development Status :: 1 - Planning',
+           'Environment :: Web Environment',
+           'Framework :: Flask',
+           'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+           'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+      ),
       zip_safe=False)
